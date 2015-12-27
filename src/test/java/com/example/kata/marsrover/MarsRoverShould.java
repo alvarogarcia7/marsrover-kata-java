a943a4003953c674dbe01d22825d328e0fed8628
alvaro garcia <derecerca@gmail.com>
2015-12-28 00:42:59 +0100

    green

--- src/test/java/com/example/kata/marsrover/MarsRoverShould.java
+++ src/test/java/com/example/kata/marsrover/MarsRoverShould.java
@@ -21,7 +21,28 @@ public class MarsRoverShould {
 	}
 
 	private static class Position{
+		private final int x;
+		private final int y;
+		private final Direction direction;
+
 		public Position(int x, int y, Direction facing){
+			this.x = x;
+			this.y = y;
+			this.direction = facing;
+		}
+
+		@Override
+		public boolean equals (final Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+
+			final Position position = (Position) o;
+
+			if (x != position.x) return false;
+			if (y != position.y) return false;
+			if (direction != position.direction) return false;
+
+			return true;
 		}
 	}
 
