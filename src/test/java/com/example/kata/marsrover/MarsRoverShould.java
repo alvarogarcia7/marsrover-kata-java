4c3cc6e663bf30347e446721db7171ef34422beb
alvaro garcia <derecerca@gmail.com>
2015-12-28 22:04:17 +0100

    red: start from the West

--- src/test/java/com/example/kata/marsrover/MarsRoverShould.java
+++ src/test/java/com/example/kata/marsrover/MarsRoverShould.java
@@ -30,6 +30,16 @@ public class MarsRoverShould {
 	}
 
 	@Test
+	public void start_facing_from_the_weast () {
+
+		rover = facingWestAt(3, 2);
+
+		rover.obey("f");
+
+		assertThat(rover, is(facingWestAt(2, 2)));
+	}
+
+	@Test
 	public void move_twice_from_another_direction () {
 
 		rover = facingEastAt(1, 2);
@@ -45,6 +55,10 @@ public class MarsRoverShould {
 
 	public static Rover facingEastAt(int x, int y) {
 		return landRoverAt(x, y, Direction.E);
+	}
+
+	public static Rover facingWestAt (int x, int y) {
+		return landRoverAt(x, y, Direction.W);
 	}
 
 	private static Rover landRoverAt(int x, int y, Direction facing){
