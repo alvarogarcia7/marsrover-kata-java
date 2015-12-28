ae385002debf34ef964598ca55a5bc35b58ba794
alvaro garcia <derecerca@gmail.com>
2015-12-28 01:29:34 +0100

    refactor: experiment comparing the whole object so the encapsulation is not broken

--- src/test/java/com/example/kata/marsrover/MarsRoverShould.java
+++ src/test/java/com/example/kata/marsrover/MarsRoverShould.java
@@ -15,7 +15,7 @@ public class MarsRoverShould {
 
 		rover.obey("ff");
 
-		assertThat(rover.position(), is(facingNorthAt(0, 2)));
+		assertThat(rover, is(roverFacingNorthAt(0, 2)));
 	}
 
 	@Test
@@ -83,6 +83,23 @@ public class MarsRoverShould {
 
 		public void obey(String commands) {
 			position = position.increaseY(commands.length());
+		}
+
+		@Override
+		public boolean equals (final Object o) {
+			if (this == o) return true;
+			if (o == null || getClass() != o.getClass()) return false;
+
+			final Rover rover = (Rover) o;
+
+			if (position != null ? !position.equals(rover.position) : rover.position != null) return false;
+
+			return true;
+		}
+
+		@Override
+		public int hashCode () {
+			return position != null ? position.hashCode() : 0;
 		}
 	}
 }
